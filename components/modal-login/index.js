6c7e6105d85629459d71f7eddc99ce09bc480474
ljybill <ljybill@aliyun.com>
2019-12-24 22:11:43 +0800

    登录注册弹框

--- components/modal-login/index.js
+++ components/modal-login/index.js
@@ -3,21 +3,38 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {
-
-  },
+  properties: {},
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    currentIdx: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    handleChange(evt) {
+      const { current } = evt.detail;
+      this.setData({
+        currentIdx: current
+      });
+    },
+    handleTabTap(evt) {
+      const { tabIdx } = evt.currentTarget.dataset;
+      if (this.data.currentIdx !== tabIdx) {
+        this.setData({
+          currentIdx: tabIdx
+        });
+      }
+    },
+    handleLoginSubmit(evt) {
+      console.log(evt);
+    },
+    handleRegisterSubmit(evt) {
+      console.log(evt);
+    }
   }
-})
+});
--- model/mock.js
+++ model/mock.js
@@ -84,3 +84,194 @@ export const userInfo = {
   ],
   isAdmin: false
 };
+
+export const message = {
+  _id: "5e02149af33edd24c4935f22",
+  createTime: "2019-12-24T13:37:30.250Z",
+  from: {
+    tag: "随便",
+    _id: "5adad39655703565e79043b8",
+    username: "222222",
+    avatar: "//cdn.suisuijiang.com/Avatar/5adad39655703565e79043b8_1575272383033"
+  },
+  to: "5adacdcfa109ce59da3e83d3",
+  type: "text",
+  content: "#(阴险)尝试性抛出一个问题"
+};
+
+export const messageList = [
+  {
+    messages: [
+      {
+        type: "text",
+        content: "没啥好吃的",
+        _id: "5e01fa39f33edd24c4935ea1",
+        from: {
+          _id: "5bd6770d5d03f62414b77d37",
+          username: "StarYang",
+          avatar: "//cdn.suisuijiang.com/Avatar/5bd6770d5d03f62414b77d37_1567136284489"
+        },
+        createTime: "2019-12-24T11:44:57.895Z"
+      },
+      {
+        type: "text",
+        content: "还是大奶的图点名主题",
+        _id: "5e01fa4bf33edd24c4935ea2",
+        from: {
+          _id: "5bd6770d5d03f62414b77d37",
+          username: "StarYang",
+          avatar: "//cdn.suisuijiang.com/Avatar/5bd6770d5d03f62414b77d37_1567136284489"
+        },
+        createTime: "2019-12-24T11:45:15.137Z"
+      },
+      {
+        type: "text",
+        content: "这是个打炮的日子",
+        _id: "5e01fa53f33edd24c4935ea3",
+        from: {
+          _id: "5bd6770d5d03f62414b77d37",
+          username: "StarYang",
+          avatar: "//cdn.suisuijiang.com/Avatar/5bd6770d5d03f62414b77d37_1567136284489"
+        },
+        createTime: "2019-12-24T11:45:23.592Z"
+      },
+      {
+        type: "text",
+        content: "#(乖)",
+        _id: "5e01faddf33edd24c4935eaa",
+        from: {
+          _id: "5adad39555703565e7904025",
+          username: "mdzz",
+          avatar: "//cdn.suisuijiang.com/Avatar/5adad39555703565e7904025_1531379412881"
+        },
+        createTime: "2019-12-24T11:47:41.751Z"
+      },
+      {
+        type: "text",
+        content: "送了个苹果 + 车厘子",
+        _id: "5e01fae9f33edd24c4935eab",
+        from: {
+          _id: "5adad39555703565e7904025",
+          username: "mdzz",
+          avatar: "//cdn.suisuijiang.com/Avatar/5adad39555703565e7904025_1531379412881"
+        },
+        createTime: "2019-12-24T11:47:53.049Z"
+      },
+      {
+        type: "text",
+        content: "#(怒)送什么，玩switch去",
+        _id: "5e01fc0df33edd24c4935eb2",
+        from: {
+          _id: "5bd6770d5d03f62414b77d37",
+          username: "StarYang",
+          avatar: "//cdn.suisuijiang.com/Avatar/5bd6770d5d03f62414b77d37_1567136284489"
+        },
+        createTime: "2019-12-24T11:52:45.515Z"
+      },
+      {
+        type: "text",
+        content: "#(阴险)",
+        _id: "5e01fc5af33edd24c4935eb3",
+        from: {
+          _id: "5b4ee8321b53ec11c8505de5",
+          username: "zhcxk1998",
+          avatar: "//cdn.suisuijiang.com/Avatar/5b4ee8321b53ec11c8505de5_1562211693929"
+        },
+        createTime: "2019-12-24T11:54:02.146Z"
+      },
+      {
+        type: "text",
+        content: "#(呵呵)",
+        _id: "5e01fcccf33edd24c4935eb5",
+        from: {
+          _id: "5adad39555703565e7904025",
+          username: "mdzz",
+          avatar: "//cdn.suisuijiang.com/Avatar/5adad39555703565e7904025_1531379412881"
+        },
+        createTime: "2019-12-24T11:55:56.172Z"
+      },
+      {
+        type: "text",
+        content: "文明6只要160？",
+        _id: "5e01fcf9f33edd24c4935eb6",
+        from: {
+          _id: "5adad39555703565e7904025",
+          username: "mdzz",
+          avatar: "//cdn.suisuijiang.com/Avatar/5adad39555703565e7904025_1531379412881"
+        },
+        createTime: "2019-12-24T11:56:41.766Z"
+      },
+      {
+        type: "text",
+        content: "190",
+        _id: "5e01fcfef33edd24c4935eb7",
+        from: {
+          _id: "5adad39555703565e7904025",
+          username: "mdzz",
+          avatar: "//cdn.suisuijiang.com/Avatar/5adad39555703565e7904025_1531379412881"
+        },
+        createTime: "2019-12-24T11:56:46.231Z"
+      },
+      {
+        type: "text",
+        content: "?",
+        _id: "5e0202eef33edd24c4935ece",
+        from: {
+          _id: "5e0202e3f33edd24c4935ecd",
+          username: "alory",
+          avatar: "//cdn.suisuijiang.com/fiora/avatar/11.jpg"
+        },
+        createTime: "2019-12-24T12:22:06.576Z"
+      },
+      {
+        type: "text",
+        content: "#(呵呵) 为什么我编译个node半小时都编译不好",
+        _id: "5e02046ef33edd24c4935ed4",
+        from: {
+          _id: "5adad39555703565e7904025",
+          username: "mdzz",
+          avatar: "//cdn.suisuijiang.com/Avatar/5adad39555703565e7904025_1531379412881"
+        },
+        createTime: "2019-12-24T12:28:30.341Z"
+      },
+      {
+        type: "text",
+        content: "如何从ali内网考贝代码#(阴险)",
+        _id: "5e0204d6f33edd24c4935ed8",
+        from: {
+          _id: "5c948ae75bef9a1505b77d20",
+          username: "绿绿酱",
+          avatar: "//cdn.suisuijiang.com/Avatar/5c948ae75bef9a1505b77d20_1557110361060"
+        },
+        createTime: "2019-12-24T12:30:14.178Z"
+      },
+      {
+        type: "text",
+        content: "#(真棒)",
+        _id: "5e0208c2f33edd24c4935eec",
+        from: {
+          _id: "5e0208b8f33edd24c4935eea",
+          username: "352000",
+          avatar: "//cdn.suisuijiang.com/fiora/avatar/5.jpg"
+        },
+        createTime: "2019-12-24T12:46:58.451Z"
+      },
+      {
+        type: "text",
+        content: "#(滑稽)",
+        _id: "5e020c26f33edd24c4935ef9",
+        from: {
+          _id: "5dfb6c1df33edd24c49322fd",
+          username: "mikumikujun",
+          avatar: "//cdn.suisuijiang.com/Avatar/5dfb6c1df33edd24c49322fd_1576758690571"
+        },
+        createTime: "2019-12-24T13:01:26.072Z"
+      }
+    ],
+    _id: "5adacdcfa109ce59da3e83d3",
+    name: "fiora",
+    avatar: "//cdn.suisuijiang.com/GroupAvatar/5adad39555703565e7903f78_1546952226984",
+    createTime: "2018-04-21T05:36:15.876Z",
+    creator: "5ca09cde11f8580ffc4d13dc"
+  }
+];
